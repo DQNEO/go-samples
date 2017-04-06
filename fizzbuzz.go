4eb88012513f2570c5608c4166493ac518ff9e4d
DQNEO <dqneoo@gmail.com>
2017-04-07 00:11:28 +0900

    refactor

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,18 +1,24 @@
 package main
 
 import "fmt"
+import "strconv"
+
+func logick(i int) string {
+    if (i % 3 ) == 0 && (i % 5) == 0 {
+        return "fizz buzz"
+    } else if (i % 3 ) == 0 {
+        return "fizz"
+    } else if (i % 5) == 0 {
+        return "buzz"
+    } else {
+        return strconv.Itoa(i)
+    }
+}
 
 func main() {
     var start,end int = 1,15
     for i := start; i <= end ; i++ {
-        if (i % 3 ) == 0 && (i % 5) == 0 {
-            fmt.Println("fizz buzz")
-        } else if (i % 3 ) == 0 {
-            fmt.Println("fizz")
-        } else if (i % 5) == 0 {
-            fmt.Println("buzz")
-        } else {
-            fmt.Println(i)
-        }
+        s := logick(i)
+        fmt.Println(s)
     }
 }
