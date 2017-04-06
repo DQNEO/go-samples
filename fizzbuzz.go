ef09eda224fa701347630fd67471c67786bb1b6d
DQNEO <dqneoo@gmail.com>
2017-04-07 00:04:42 +0900

    impl fizz buzz

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -5,6 +5,14 @@ import "fmt"
 func main() {
     var start,end int = 1,15
     for i := start; i <= end ; i++ {
-        fmt.Println(i)
+        if (i % 3 ) == 0 && (i % 5) == 0 {
+            fmt.Println("fizz buzz")
+        } else if (i % 3 ) == 0 {
+            fmt.Println("fizz")
+        } else if (i % 5) == 0 {
+            fmt.Println("buzz")
+        } else {
+            fmt.Println(i)
+        }
     }
 }
