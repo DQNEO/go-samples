dd69c4976807cbefad11dcfdc21422050742d04e
DQNEO <dqneoo@gmail.com>
2020-01-15 18:39:44 +0900

    Implemented tokenizer

--- nlp/main.go
+++ nlp/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 )
+
 // Take a English text from stdin, and analyze it
 // Usage: go run nlp/main.go < ,/english.txt
 
@@ -15,7 +16,41 @@ func getText() []byte {
 	return bytes
 }
 
+func tokenize(bytes []byte) []string {
+	var tokens []string
+	var token []byte
+	var isInWord bool
+	for _, b := range bytes {
+		switch b {
+		case ' ', '\n', '.', '"', ',' , '\'', '?', '!', '(', ')':
+			if !isInWord {
+				continue
+			}
+			tokens = append(tokens, string(token))
+			token = nil
+			isInWord = false
+		default:
+			isInWord = true
+			byte := b
+			if 'A' <= b && b <= 'Z' {
+				byte = b - 'A' + 'a'
+			}
+			token = append(token, byte)
+		}
+	}
+
+	return tokens
+}
+
+func printTokens(tokens []string) {
+	for _, tok := range tokens {
+		fmt.Printf("%s\n", tok)
+	}
+}
+
 func main() {
 	bytes := getText()
-	fmt.Printf("%s", bytes)
+	tokens := tokenize(bytes)
+	fmt.Printf("number of tokens: %d\n", len(tokens))
+	printTokens(tokens)
 }
