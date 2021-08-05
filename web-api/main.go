205aa7035ea90eb27adcd54d167b1d117b5c826b
DQNEO <dqneoo@gmail.com>
2021-08-05 19:54:25 +0900

    Unwrap high level funcs

--- web-api/main.go
+++ web-api/main.go
@@ -10,8 +10,18 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
+const port = "8080"
+const addr = ":" + port
+
 func main() {
-	http.HandleFunc("/", handler)
-	err := http.ListenAndServe(":8080", nil)
+	http.DefaultServeMux.HandleFunc("/", handler)
+	http.DefaultServeMux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello\n"))
+	})
+	log.Printf("access http://localhost:%s/ from any client", port)
+	server := http.Server{
+		Addr: addr,
+	}
+	err := server.ListenAndServe()
 	log.Fatal(err)
 }
