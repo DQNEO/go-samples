af7c102e5a3e1d06d830062404f1d7ac6da8644f
DQNEO <dqneoo@gmail.com>
2022-08-10 19:53:42 +0900

    Merge multi rss feeds into one timeline

--- podcast-parser/main.go
+++ podcast-parser/main.go
@@ -1,9 +1,10 @@
-// go run . < data/the-daily.xml > a.html
+// go run . > out/index.html
 package main
 
 import (
 	"html/template"
 	"os"
+	"sort"
 	"time"
 
 	"github.com/mmcdole/gofeed"
@@ -16,10 +17,21 @@ type Podcast struct {
 }
 
 type Item struct {
+	Id        string
 	Title     string
 	AudioURL  string
 	Duration  string
-	Published *time.Time
+	Published time.Time
+}
+
+type LineItem struct {
+	Id              string
+	Title           string
+	AudioURL        string
+	Duration        string
+	Published       time.Time
+	ChannelImageURL string
+	ChannelTitle    string
 }
 
 func convPostcast(feed *gofeed.Feed) *Podcast {
@@ -28,10 +40,11 @@ func convPostcast(feed *gofeed.Feed) *Podcast {
 		ImageURL: feed.Image.URL,
 	}
 	var items []*Item
-	for _, item := range feed.Items[0:5] {
+	for _, item := range feed.Items[0:3] {
 		items = append(items, &Item{
+			Id:        item.GUID,
 			Title:     item.Title,
-			Published: item.PublishedParsed,
+			Published: *item.PublishedParsed,
 			AudioURL:  item.Enclosures[0].URL,
 			Duration:  item.ITunesExt.Duration,
 		})
@@ -40,48 +53,75 @@ func convPostcast(feed *gofeed.Feed) *Podcast {
 	return p
 }
 
-func main() {
-	fp := gofeed.NewParser()
-	feed, _ := fp.Parse(os.Stdin)
-	//fmt.Print(feed)
-	cast := convPostcast(feed)
-	renderHTML(cast)
+func getPodcasts() []*Podcast {
+	d, err := os.Open("./data")
+	if err != nil {
+		panic(err)
+	}
+
+	rssFiles, err := d.Readdirnames(0)
+	if err != nil {
+		panic(err)
+	}
+	var casts []*Podcast
+	for _, r := range rssFiles {
+		fpath := "./data/" + r
+		f, err := os.Open(fpath)
+		if err != nil {
+			panic(err)
+		}
+		fp := gofeed.NewParser()
+		feed, _ := fp.Parse(f)
+		cast := convPostcast(feed)
+		casts = append(casts, cast)
+	}
+	return casts
 }
 
-func renderHTML(cast *Podcast) {
-	t, err := template.New("webpage").Parse(tpl)
+func main() {
+	casts := getPodcasts()
+	var items []*LineItem
+	for _, c := range casts {
+		for _, i := range c.Items {
+			li := &LineItem{
+				Id:              i.Id,
+				Title:           i.Title,
+				AudioURL:        i.AudioURL,
+				Duration:        i.Duration,
+				Published:       i.Published,
+				ChannelImageURL: c.ImageURL,
+				ChannelTitle:    c.Title,
+			}
+			items = append(items, li)
+
+		}
+	}
+	// Sort for timeline view (newer is upper)
+	sort.Slice(items, func(i, j int) bool {
+		return items[i].Published.After(items[j].Published)
+	})
+	renderItems(items)
+}
+
+func renderItems(items []*LineItem) {
+	t, err := template.New("webpage").Parse(htmlTemplateItems)
+	check(err)
+	type data struct {
+		Items []*LineItem
+	}
+	d := data{
+		Items: items,
+	}
+	err = t.Execute(os.Stdout, d)
+	check(err)
+}
+
+func renderPodcast(cast *Podcast) {
+	t, err := template.New("webpage").Parse(htmlTemplate)
 	check(err)
 	err = t.Execute(os.Stdout, cast)
 	check(err)
 }
-
-const tpl = `
-<!DOCTYPE html>
-<html>
-
-	<head>
-	<meta charset="UTF-8">
-	<title>Listening All The Time</title>
-	</head>
-
-	<body>
-	<div>
-	<h1><img src="{{.ImageURL}}" width=64 height=64> {{.Title}}</h1>
-	
-	</div>
-	<ul>
-	{{range .Items}}
-		<li>
- 		{{.Published}} - {{ .Title }} [{{.Duration}}]
-		<audio controls	src="{{.AudioURL}}">Your browser does not support the audio element.</audio>
-		</li>
-	{{else}}
-		<div><strong>no rows</strong></div>
-	{{end}}
-	<ul>
-	</body>
-</html>
-`
 
 var check = func(err error) {
 	if err != nil {
---
+++ podcast-parser/tpl.go
@@ -0,0 +1,75 @@
+package main
+
+const htmlTemplate = `
+<!DOCTYPE html>
+<html>
+
+	<head>
+	<meta charset="UTF-8">
+	<title>Listening English All The Time</title>
+	<script>
+	document.addEventListener("DOMContentLoaded", function(){
+		document.body.addEventListener("click", ()=>{
+		var audios = document.getElementsByTagName("audio");
+		audios[0].play();
+		})
+	});
+	</script>
+	</head>
+
+	<body>
+	<div>
+	<h1><img src="{{.ImageURL}}" width=64 height=64> {{.Title}}</h1>
+	
+	</div>
+	<ul>
+	{{range .Items}}
+		<li>
+ 		{{.Published}} - {{ .Title }} [{{.Duration}}]
+		<audio id="{{.Id}}" controls src="{{.AudioURL}}">Your browser does not support the audio element.</audio>
+		</li>
+	{{else}}
+		<div><strong>no rows</strong></div>
+	{{end}}
+	<ul>
+	</body>
+</html>
+`
+
+const htmlTemplateItems = `
+<!DOCTYPE html>
+<html>
+
+	<head>
+	<meta charset="UTF-8">
+	<title>Listening English All The Time</title>
+	<script>
+	document.addEventListener("DOMContentLoaded", function(){
+		document.body.addEventListener("click", ()=>{
+		var audios = document.getElementsByTagName("audio");
+		audios[0].play();
+		})
+	});
+	</script>
+	</head>
+
+	<body>
+	<div>
+	<h1>Listening English All The Time</h1>
+	</div>
+	<ul>
+	{{range .Items}}
+		<li>
+		<img src="{{.ChannelImageURL}}" width=64 height=64> 
+ 		{{.Published}} 
+		<audio id="{{.Id}}" controls src="{{.AudioURL}}">Your browser does not support the audio element.</audio>
+		- {{ .Title }} - {{ .ChannelTitle }}
+		[{{.Duration}}]
+		</li>
+	{{else}}
+		<div><strong>no rows</strong></div>
+	{{end}}
+	<ul>
+	</body>
+</html>
+`
