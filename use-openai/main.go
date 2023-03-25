1d77d6b5471f14278aac505fda06b684b2f08df6
DQNEO <dqneoo@gmail.com>
2023-03-25 12:25:33 +0900

    Add more examples

--- use-openai/main.go
+++ use-openai/main.go
@@ -9,7 +9,32 @@ import (
 )
 
 func main() {
-	client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
+	CreateChatCompletion()
+	CreateCompletion()
+}
+
+var client = openai.NewClient(os.Getenv("OPENAI_API_KEY"))
+
+func CreateCompletion() {
+	fmt.Printf("CreateCompletion: ")
+	ctx := context.Background()
+
+	req := openai.CompletionRequest{
+		Model:     openai.GPT3Ada,
+		MaxTokens: 5,
+		Prompt:    "Lorem ipsum",
+	}
+	resp, err := client.CreateCompletion(ctx, req)
+	if err != nil {
+		fmt.Printf("Completion error: %v\n", err)
+		return
+	}
+	fmt.Println(resp.Choices[0].Text)
+}
+
+func CreateChatCompletion() {
+	fmt.Printf("CreateChatCompletion: ")
+
 	ctx := context.Background()
 	resp, err := client.CreateChatCompletion(
 		ctx,
