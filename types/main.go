c059eb4a24382ef5af6694cc124073c8e1a8c549
DQNEO <dqneoo@gmail.com>
2023-07-05 00:10:00 +0900

    Look into local scope

--- types/main.go
+++ types/main.go
@@ -10,8 +10,7 @@ import (
 	"log"
 )
 
-const hello = `package main
-
+const code = `package main
 import "fmt"
 
 type MyInt int
@@ -19,9 +18,12 @@ type MyMyInt MyInt
 var i MyMyInt = 1
 var x = 123
 var s = "abc"
+var ch = 'a' 
 
 func main() {
-        fmt.Println(i)
+	var i1 = 456
+	i2 := 789
+	fmt.Println(i1, i2)
 }`
 
 func main() {
@@ -30,7 +32,7 @@ func main() {
 	// Parse the input string, []byte, or io.Reader,
 	// recording position information in fset.
 	// ParseFile returns an *ast.File, a syntax tree.
-	f, err := parser.ParseFile(fset, "hello.go", hello, 0)
+	f, err := parser.ParseFile(fset, "code.go", code, 0)
 	if err != nil {
 		log.Fatal(err) // parse error
 	}
@@ -42,7 +44,10 @@ func main() {
 
 	// Type-check the package containing only file f.
 	// Check returns a *types.Package.
-	pkg, err := conf.Check("cmd/hello", fset, []*ast.File{f}, nil)
+	info := types.Info{
+		Scopes: make(map[ast.Node]*types.Scope),
+	}
+	pkg, err := conf.Check("pseudo", fset, []*ast.File{f}, &info)
 	if err != nil {
 		log.Fatal(err) // type error
 	}
@@ -52,12 +57,19 @@ func main() {
 	fmt.Printf("Package.Imports(): %s\n", pkg.Imports())
 	fmt.Printf("Package,Scope():   %s\n", pkg.Scope())
 
-	fmt.Println("[Iterating Names]")
+	fmt.Println("[Names in the package scope]")
 	for i, name := range pkg.Scope().Names() {
 		fmt.Printf("  %d:  name=%s\n", i, name)
 		obj := pkg.Scope().Lookup(name)
 		objType := obj.Type()
-		fmt.Printf("    obj: kind=%T, name=%s, String()='%s' type=%s, Underlying=%s\n",
-			obj, obj.Name(), obj.String(), objType.String(), objType.Underlying().String())
+		fmt.Printf("    obj: kind=%T, type=%s, Underlying=%s, String()='%s' \n",
+			obj, objType.String(), objType.Underlying().String(), obj.String())
+	}
+
+	fmt.Println("[Looking into local scopes]")
+	for node, scope := range info.Scopes {
+		if _, isFunc := node.(*ast.FuncType); isFunc {
+			fmt.Printf("%s: node=%T, scope=%s\n", node.Pos(), node, scope)
+		}
 	}
 }
