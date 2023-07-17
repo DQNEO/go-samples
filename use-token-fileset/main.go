2b4585d7fa9e76eeb1f315606c9e01ec5a1f6a09
DQNEO <dqneoo@gmail.com>
2023-07-17 23:43:06 +0900

    Add fset position example

--- use-token-fileset/main.go
+++ use-token-fileset/main.go
@@ -2,16 +2,42 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
+	"go/parser"
 	"go/token"
 )
 
 func main() {
 	test1()
+	test2()
+}
+
+func test2() {
+	fmt.Println("===== test2")
+	fset := token.NewFileSet()
+	astFile, err := parser.ParseFile(fset, "./testdata/hello.go", nil, 0)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("astFile.Package:%d (position=%s)\n", astFile.Package, fset.Position(astFile.Package))
+	fmt.Printf("Scope:main=%#v\n", astFile.Scope.Objects["main"])
+	fmt.Println("Decls ---")
+	for _, decl := range astFile.Decls {
+		fmt.Printf("  %#v\n", decl)
+		funcDecl, isFunc := decl.(*ast.FuncDecl)
+		if isFunc {
+			for _, stmt := range funcDecl.Body.List {
+				fmt.Printf("  stmt:%#v (%s)\n", stmt, fset.Position(stmt.Pos()))
+			}
+		}
+	}
+
 }
 
 // Trying this article
 // https://abhinavg.net/2019/03/28/understanding-token-pos/
 func test1() {
+	fmt.Println("===== test1")
 	fset := token.NewFileSet()
 	fset.AddFile("1.go", -1, 10)      // base => 12
 	fset.AddFile("2.go", -1, 15)      // base => 28
